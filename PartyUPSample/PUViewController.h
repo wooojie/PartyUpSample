90994f9848f21c8462ad07d315da72c309b0c2bc
Jie Wu <ben.wujie@gmail.com>
2012-03-11 19:52:20 -0700

    Initial Commit

---
+++ PartyUPSample/PUViewController.h
@@ -0,0 +1,13 @@
+//
+//  PUViewController.h
+//  PartyUPSample
+//
+//  Created by Jie Wu on 3/11/12.
+//  Copyright (c) 2012 NYU poly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PUViewController : UIViewController
+
+@end
---
+++ PartyUPSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PartyUPSample
+//
+//  Created by Jie Wu on 3/11/12.
+//  Copyright (c) 2012 NYU poly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PUAppDelegate class]));
+    }
+}
